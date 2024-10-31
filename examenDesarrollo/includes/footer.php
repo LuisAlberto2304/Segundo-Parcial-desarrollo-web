2f606e942aa0145739ec17086ce396eb478ba43a
LuisAlberto2304 <0323105921@ut-tijuana.edu.mx>
2024-10-30 18:14:21 -0700

    Add files via upload

---
+++ examenDesarrollo/includes/footer.php
@@ -0,0 +1,21 @@
+<section>
+        <footer>
+            <div>
+                <nav>
+                <a href="index.php">Inicio</a>
+                <a href="crearSeller.php">Crear anunciante</a>
+                <a href="crearPropierties.php">Crear propiedad</a>
+                <a href="crearSales.php">Realizar Venta</a>
+                </nav>
+            </div>
+            <div>
+                <p>
+                    Todos los Derechos Rervados by: Yo
+                </p>
+            </div>
+        </footer>
+    </section>
+
+
+</body>
+</html>
---
+++ examenDesarrollo/index.php
@@ -0,0 +1,7 @@
+<?php include "includes/header.php" ?>
+
+<h2>Ventas de casas y departamenos exclusivos de lujo</h2>
+
+
+
+<?php include "includes/footer.php" ?>
